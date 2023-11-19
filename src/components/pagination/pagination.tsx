ee5f6bfe698eb4a979317d59c1628de308952e54
Pedro Reis <itspdro@outlook.com>
2023-11-18 22:39:13 -0300

    [+]feat: add pagination component.

---
+++ src/components/pagination/pagination.tsx
@@ -0,0 +1,54 @@
+import { ChevronLeft, ChevronRight } from '../icons';
+import './pagination.css';
+
+interface PaginationProps {
+  totalPages: number
+  setCurPage: (page: number) => void
+  curPage: number
+}
+
+function Pagination(
+  {
+    totalPages,
+    setCurPage,
+    curPage
+  }: PaginationProps
+) {
+
+  return (
+    <nav className="pagination_container">
+    <ul className="pagination_container__list">
+      <li className="pagination_container__list-item">
+        <button className="pagination_container__list-item__button"
+          disabled={curPage === 1}
+          onClick={() => curPage > 1 && setCurPage(curPage - 1)}
+        >
+          <ChevronLeft />
+        </button>
+      </li>
+
+      {Array.from({ length: totalPages }).map((_, index) => (
+          <li key={index} className="pagination_container__list-item">
+            <button
+              className={`pagination_container__list-item__button ${curPage === index + 1 ? 'active' : ''}`}
+              onClick={() => setCurPage(index + 1)}
+            >
+              {index + 1}
+            </button>
+          </li>
+      ))}
+      <li className="results_container_pagination__list-item">
+        <button className="pagination_container__list-item__button"
+          disabled={curPage === totalPages}
+          onClick={() => curPage < totalPages && setCurPage(curPage + 1)}
+        >
+          <ChevronRight />
+        </button>
+      </li>
+    </ul>
+
+  </nav>
+  )
+}
+
+export default Pagination
