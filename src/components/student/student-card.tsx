c90701ae7b59095e708bc13d53b9f92081cdb67b
Pedro Reis <itspdro@outlook.com>
2023-11-18 22:40:09 -0300

    [+]style: update table css.

--- src/components/student/student-card.tsx
+++ src/components/student/student-card.tsx
@@ -1,19 +1,31 @@
+import './student-card.css';
 import { Aluno } from "@/types/Aluno";
 
-function StudentCard({nome, email, dataCancelamento}:  Aluno) {
+function StudentCard({
+  codMatricula,
+  nome,
+  email,
+  sexo,
+  dataCancelamento
+}:  Aluno) {
   return (
-    <div className="student-card_container">
-      <span className={`student-card_container__status ${dataCancelamento ? 'red' : 'green'}`}>
-      </span>
-      <div className="student-card_container__info">
-        <div className="student-card_container__info__name">
-          {nome}
-        </div>
-        <div className="student-card_container__info__email">
-          {email}
-        </div>
-      </div>
-    </div>
+    <tr className="student-card_container">
+      <td className="student-card_container__matricula">
+        {codMatricula}
+      </td>
+      <td className="student-card_container__name">
+        {nome}
+      </td>
+      <td className="student-card_container__email">
+        {email}
+      </td>
+      <td className="student-card_container__sexo">
+        {sexo}
+      </td>
+      <td className="student-card_container__status">
+        {dataCancelamento ? 'Inativo' : 'Ativo'}
+      </td>
+    </tr>
   )
 }
 
